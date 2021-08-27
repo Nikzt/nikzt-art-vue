6c9df717b43a0bc67495f911291eeda8576dfad6
Nikzt <bence.linder@gmail.com>
2021-08-26 20:24:25 -0400

    on click image previews and refactoring

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,119 @@
+<template>
+  <splide :options="options" @splide:click="onSlideClick">
+    <splide-slide v-for="url in imageUrls" v-bind:key="url">
+      <img :src="`${url}`" />
+    </splide-slide>
+  </splide>
+  <image-preview :showPreview="showPreview" :imageUrl="selectedImageUrl" @click="onPreviewClick" />
+</template>
+
+<script>
+import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import ImagePreview from "./ImagePreview.vue";
+import { ref } from "vue";
+
+export default {
+  name: "ImageGallery",
+  components: { Splide, SplideSlide, ImagePreview },
+  props: {
+    imageUrls: Array,
+  },
+  setup(props) {
+    const options = {
+      type: "loop",
+      perPage: 3,
+      perMove: 1,
+      focus: "center",
+      trimSpace: true,
+      gap: "2.5rem",
+      lazyLoad: true,
+      cover: true,
+      slideFocus: true,
+      padding: {
+        left: 0,
+      },
+      breakpoints: {
+        750: {
+          perPage: 2,
+          focus: 0,
+        },
+        400: {
+          perPage: 1,
+          focus: 0,
+        },
+      },
+    };
+
+    const showPreview = ref(false);
+    const selectedImageUrl = ref("");
+
+    const onSlideClick = (splide, slide) => {
+      showPreview.value = true;
+      const idx =
+        slide.index >= 0 && slide.index < props.imageUrls.length
+          ? slide.index
+          : slide.realIndex;
+      selectedImageUrl.value = props.imageUrls[idx];
+    };
+
+    const onPreviewClick = () => {
+      showPreview.value = false;
+    }
+
+    return {
+      options,
+      showPreview,
+      selectedImageUrl,
+      onSlideClick,
+      onPreviewClick
+    };
+  },
+};
+</script>
+
+<style>
+.splide {
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+  max-width: 860px;
+  height: 40vh;
+  min-height: 400px;
+}
+
+.splide__track {
+  margin: 0 auto 0 auto;
+  border-radius: 20px;
+}
+
+.splide__arrow--next {
+  right: -3em;
+}
+
+.splide__arrow--prev {
+  left: -3em;
+}
+
+.splide__pagination {
+  bottom: -2em;
+}
+
+.splide__slide {
+  max-width: 20rem;
+  min-width: 14rem;
+  border-radius: 20px;
+  height: 40vh;
+  min-height: 400px;
+  cursor: pointer;
+  transition: opacity 0.08s linear;
+}
+
+.splide__slide:hover {
+  opacity: 0.8;
+  transition: opacity 0.08s linear;
+}
+img {
+  height: 100%;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,89 +2,31 @@
   <div>
     <NikztLogo class="noselect" />
 
-    <splide :options="options">
-      <splide-slide>
-        <img src="../assets/gradient-portrait-1.png" />
-      </splide-slide>
-      <splide-slide>
-        <img src="../assets/gradient-portrait-2.png" />
-      </splide-slide>
-      <splide-slide>
-        <img src="../assets/sample-artwork-1.jpg" />
-      </splide-slide>
-      <splide-slide>
-        <img src="../assets/sample-artwork-4.jpg" />
-      </splide-slide>
-      <splide-slide>
-        <img src="../assets/sample-artwork-5.jpg" />
-      </splide-slide>
-    </splide>
+    <ImageGallery :imageUrls="imageUrls"/>
   </div>
 </template>
 
 <script>
-import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
 import NikztLogo from "../components/NikztLogo.vue";
-import { Splide, SplideSlide } from "@splidejs/vue-splide";
-import {ref} from 'vue';
+import ImageGallery from "../components/ImageGallery.vue";
 
 export default {
   name: "HomePage",
-  components: { NikztLogo, Splide, SplideSlide },
+  components: { NikztLogo, ImageGallery },
   setup() {
-    const options = ref({
-      type: "loop",
-      perPage: window.innerWidth > 750 ? 3 : 1,
-      perMove: 1,
-      focus: window.innerWidth > 750 ? "center" : 0,
-      trimSpace: true,
-      gap: "2.5rem",
-      padding: {
-        left: 0
-      },
-      lazyLoad: true,
-      cover: true,
-    });
-    return { options };
+
+    const imageUrls = [
+      require("../images/gradient-portrait-1.png"),
+      require("../images/gradient-portrait-2.png"),
+      require("../images/sample-artwork-1.jpg"),
+      require("../images/sample-artwork-2.jpg"),
+      require("../images/sample-artwork-3.jpg"),
+    ]
+
+    return { imageUrls };
   },
 };
 </script>
 
 <style >
-.splide {
-  margin-left: auto;
-  margin-right: auto;
-  width: 90%;
-  max-width: 1080px;
-  height: 40vh;
-  min-height: 400px;
-}
-
-.splide__track {
-  margin: 0 auto 0 auto;
-  border-radius: 20px;
-}
-
-.splide__arrow--next {
-  right: -3em;
-}
-
-.splide__arrow--prev {
-  left: -3em;
-}
-
-.splide__pagination {
-  bottom: -2em;
-}
-
-.splide__slide {
-  max-width: 20rem;
-  min-width: 14rem;
-  border-radius: 20px;
-  height: 40vh;
-  min-height: 400px;
-}
-img {
-  height: 100%;
-}
 </style>
